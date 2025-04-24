d99f3d5c640a85d491837d4bf529f30401b78914
MarianaMinoni <123034714+MarianaMinoni@users.noreply.github.com>
2025-04-24 07:30:09 -0300

    cards

--- JS/productos copy.js
+++ JS/productos copy.js
@@ -45,42 +45,29 @@ guardarProductosLS();
 function renderProductos(){
   // let textoBusqueda = document.getElementById("textoBusqueda").value;
   let productos =  cargarProductosLS();
-  let contenido = `<div class="container">`;
+  let contenido = "";
 
 
    if(productos.length > 0 ){
 
 
     productos.forEach(producto => {
-contenido += `
 
-<div class="card-1 card-div">
-      <div class="like-icon-div">
-          <label for="card-1-like" class="like-icon-div-child">
-              <input type="checkbox" id="card-1-like">
-              <i class="far fa-heart heart-empty"></i>
-              <i class="fas fa-heart heart-fill"></i>
-          </label>
+      contenido += `<div class="col-md-3">
+      <a href="pages/verProducto.html" onclick="verProducto(${producto.id})" class="text-decoration-none">
+      <div class="card text-center border border-0">
+      <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+      <div class="card-body">
+          <p class="card-text "><b>$${producto.precio}</b></p>
+          <p >${producto.nombre}</p>
+       
       </div>
+    </div></a>
 
-      <div class="gow-img-div img-div">
-          <img src="${producto.imagen}" alt="${producto.nombre}">
-      </div>
-      <div class="text-container">
-          <h2 class="item-name">${producto.nombre}</h2>
-          <div class="pricing-and-cart">
-              <div class="pricing">
-                  <p class="current-price">${producto.precio}</p>
-              </div>
-              <i class="fas fa-shopping-cart"></i>
-          </div>
-      </div>
-    </div>
-  `;
-});
+    </div>`;
 
-
-      
+      });
+  
  
     
     document.getElementById("contenido").innerHTML = contenido;
